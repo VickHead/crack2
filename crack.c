fcb9acb24e7b854f50eaa1b8005bbc7fb2c2c8e2
Victor Galbraith <VictorG87@Gmail.com>
2019-04-25 02:21:25 +0000

    Mostly working, prints out the correct 20 hashes with seemingly random passwords

--- crack.c
+++ crack.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 #include "md5.h"
 
@@ -13,11 +15,18 @@ const int HASH_LEN=33;        // Length of MD5 hash strings
 int tryguess(char *hash, char *guess)
 {
     // Hash the guess using MD5
-
+    char *p = md5(guess, strlen(guess));
     // Compare the two hashes
-
+    //printf("%s ", hash);
+    //printf("%s\n", p);
+    
+    if (*hash == *p)
+    {
+        free(p);
+        return 1;
+    }
     // Free any malloc'd memory
-
+    free(p);
     return 0;
 }
 
@@ -28,8 +37,55 @@ int tryguess(char *hash, char *guess)
 // file.
 char **read_dictionary(char *filename, int *size)
 {
-    *size = 0;
-    return NULL;
+    int len = 0;
+    struct stat info;
+    if (stat(filename, &info) != -1)
+    {
+        len = info.st_size;
+        //printf("%d\n", len);
+    }
+    
+    char *contents = malloc(len);
+    
+    FILE *f = fopen(filename, "r");
+    if (!f)
+    {
+        printf("can't open %s for reading\n", filename);
+        exit(1);
+    }
+    
+    fread(contents, 1, len, f);
+    fclose(f);
+    
+    int count = 0;
+    char **arr = malloc(len * sizeof(char *));
+    arr[0] = contents;
+    
+    for (int i = 0; i < len; i++)
+    {
+        if (contents[i] == '\n')
+        {
+            count++;
+            contents[i] = '\0';
+        }
+    }
+    
+    //printf("arr[%d] = ", 0);
+    //puts(arr[0]);
+    
+    for (int i = 0, j = 0; i < len; i++)
+    {
+        if (contents[i] == '\0')
+        {
+            arr[j] = &contents[i+1];
+            //printf("arr[%d] = ", j + 1);
+            //puts(arr[j]);
+            j++;
+        }
+    }
+    
+    *size = count;
+    return arr;
 }
 
 
@@ -43,12 +99,39 @@ int main(int argc, char *argv[])
 
     // Read the dictionary file into an array of strings.
     int dlen;
-    char **dict = read_dictionary(NULL, NULL);
+    char **dict = read_dictionary("rockyou100.txt", &dlen);
+    //printf("%d\n", dlen);
+    //printf("%s\n", *dict);
 
     // Open the hash file for reading.
-    
+    FILE *h = fopen(argv[1], "r");
+    if (!h)
+    {
+        printf("Couldn't open hashes.txt for reading\n");
+        exit(1);
+    }
 
     // For each hash, try every entry in the dictionary.
     // Print the matching dictionary entry.
     // Need two nested loops.
+    char line[HASH_LEN];
+    while (fgets(line, HASH_LEN, h) != NULL)
+    {
+        for (int i = 0; i < dlen - 1; i++)
+        {
+            //printf("%d ", tryguess(line, dict[i]));
+            //printf("%d ", i);
+            
+            if (tryguess(line, dict[i]) == 1)
+            {
+                printf("%s ", line);
+                puts(dict[i]);
+                break;
+            }
+        }
+    }
+    
+    //free(dict[0]);
+    free(dict);
+    fclose(h);
 }
